29ef500a61cdba4716126c97aafb79ebbe5d580f
sgerlyn <samgerlyn@gmail.com>
2016-12-22 22:11:23 -0500

    Project wasn't pushed, update

---
+++ src/main/java/com/sgerlyn/donorreclaim/DonatorReclaim.java
@@ -0,0 +1,41 @@
+package com.sgerlyn.donorreclaim;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DonatorReclaim extends JavaPlugin implements Listener {
+
+    private static Permission perms;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+        setupPermissions();
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (player.hasPlayedBefore()) return;
+        String group = perms.getPrimaryGroup(player);
+        if (getConfig().contains(group)) {
+            for (String string : getConfig().getStringList(group)) {
+                Bukkit.dispatchCommand(getServer().getConsoleSender(), string.replace("{name}", player.getName()));
+            }
+        }
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+}
